d17c859627b52b04d40a618fed0e09e82375f83f
Indresh <ips.indresh@gmail.com>
2018-05-20 15:18:35 +0530

    added file reading and stats

--- lessions/lession-5.js
+++ lessions/lession-5.js
@@ -2,15 +2,33 @@
  * Working with node internal module: fs  (File system)
  */
 
-var fs  = require("fs");
-
+var fs = require("fs"),
+        path = require("path");
 
 // async reading file
-fs.readFile(__filename,"utf8",function(err,content){
+fs.readFile(__filename, "utf8", function (err, content) {
         console.log(content);
 });
 console.log("After reading file");
-var data = fs.readFileSync(__filename,"utf8");
+var data = fs.readFileSync(__filename, "utf8");
 console.log("============Got data=========");
 console.log(data);
 
+let dir = path.join(__dirname, "..", "..", "..");
+fs.readdir(dir, (err, files) => {
+        console.log("============Listing files =========");
+        console.log(files);
+        for (let file of files) {
+                fs.stat(path.join(dir, file), (err, stats) => {
+                        console.log(file, stats.isDirectory(), stats.size);
+                });
+        }
+});
+
+fs.writeFile("readme.txt", data + " this is modified data", {
+        flag: 'a'
+}, (err) => {
+        if (err) {
+                console.error("Got error", err);
+        }
+});
